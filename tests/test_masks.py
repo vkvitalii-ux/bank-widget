af97b9d1636c4466ebef9bb5e8b24f1bd7ba9e4d
Vitalii <vkvitalii@gmail.com>
2026-08-15 15:23:16 +0300

    fix: add return type to test functions

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,11 +1,11 @@
 from src.masks import get_mask_account_number, get_mask_card_number
 
 
-def test_get_mask_card_number():
+def test_get_mask_card_number() -> None:
     """Тест для проверки маскирования номера карты."""
     assert get_mask_card_number("7000792289606361") == "7000 79** **** 6361"
 
 
-def test_get_mask_account_number():
+def test_get_mask_account_number() -> None:
     """Тест для проверки маскирования номера счета."""
     assert get_mask_account_number("73654108430135874305") == "**4305"
